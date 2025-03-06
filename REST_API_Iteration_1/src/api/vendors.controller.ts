26039dee126284e2189582198f5960610cd18336
oY785QH43L <DD6G8zw0Wd@gmail.com>
2025-03-06 16:30:49 +0100

    Update

--- REST_API_Iteration_1/src/api/vendors.controller.ts
+++ REST_API_Iteration_1/src/api/vendors.controller.ts
@@ -881,7 +881,7 @@ export class VendorsController implements interfaces.Controller {
 
             await connection.models.ProductToCategory.destroy({ where: { categoryId: removeCategoryRequestBody.categoryId, productId: referenceConverted.productId } });
             await connection.close();
-            response.status(201).json({ message: `Category with ID ${removeCategoryRequestBody.categoryId} was successfully removed from product with ID ${referenceConverted.productId}!` });
+            response.status(200).json({ message: `Category with ID ${removeCategoryRequestBody.categoryId} was successfully removed from product with ID ${referenceConverted.productId}!` });
         }
         catch (err) {
 
